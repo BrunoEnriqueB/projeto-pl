082914770e9ce37dbd905f652d98edbfebef1210
Bruno Enrique <brunobaronenrique@gmail.com>
2023-11-19 13:38:23 -0300

    add usage example

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Graph from './components/Graph';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	function showResult(result) {
+		console.log(result);
+	}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<Graph
+				x={2}
+				y={3}
+				restrictions={['-1x+1y<=4', '1x+2y<=6', '2x+3y<=9']}
+				obj='max'
+				returnResult={showResult}
+			/>
+		</>
+	);
 }
 
-export default App
+export default App;
