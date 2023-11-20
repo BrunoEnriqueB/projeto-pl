125b52dbf9be0cf0359f5314b6aec48322bf43a7
LucasBegnini10 <begninilucas12@gmail.com>
2023-11-19 22:25:59 -0300

    init tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./index.css";
 import Simplex from "./components/simplex";
+import Tabs from "./components/tabs";
 
 export default function App() {
   return (
     <main>
-      <Simplex />
+      <Tabs />
     </main>
   );
 }
